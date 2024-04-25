68be713a4c57586d9e565c51c1c2166ea5ab80b0
Nguyen Thu <thub2014706@student.ctu.edu.vn>
2024-04-25 11:20:42 +0700

    Khoi tao du an

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+    // import AppHeader from "@/components/AppHeader.vue";
+    // export default {
+    //   components: {
+    //     AppHeader,
+    //   },
+    // }
+</script>
+
+<template>
+    <!-- <AppHeader /> -->
+    <div id="app">
+        <div class="container-fluid">
+          <router-view />
+        </div>
+    </div>
+</template>
+
+<style>
+    
+</style>
